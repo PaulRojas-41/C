a752aa530f926fe96fdd27dd45ef6790e015cb63
Paul Rojas <sirpaul.tapia@hotmail.com>
2025-10-16 16:36:23 +0200

    [dev_ex_bit_counter_2] Bit counter ex2 for any type of data
    - This example contains a more generic method for print bits of any type
      of data format

--- bitwise_lib.c
+++ bitwise_lib.c
@@ -1,26 +1,26 @@
-/* [dev_ex_bit_counter_1] Bit counter ex1 */
+/* [dev_ex_bit_counter_2] Bit counter ex2 for any type of data */
 #include <stdio.h>
 #include <stdint.h>
 #include "bitwise_lib.h"
 
-extern void print_bit(uint8_t x)
+extern void print_bit(size_t const x, void const *ptr)
 {
-    int bits = sizeof(x) * 8;
+    unsigned char *local_ptr = (unsigned char *)ptr;
     int mask;
-    int counter = bits - 1;
 
-    printf("Value:%d\n", x);
-
-    do{
-        mask = ((x & (1 << counter)) ? printf("%c",  '1') : printf("%c",  '0'));
-        counter--;
-    }while(counter >= 0);
+    for(int i = x - 1; i >= 0; i--) /* for each byte */
+    {
+        for(int j = 7; j >= 0; j--) /* read each bit */
+        {
+            mask = ((local_ptr[i] >> j) & 1) ? printf("%c",  '1') : printf("%c",  '0'); /* right shift and AND with 1 */
+        }
+    }
 }
 
 int main(void)
 {
-    uint32_t value = 29; // 0001 1101
-    print_bit(value);
+    int value = 0xAB110010; // 0001 1101
+    print_bit(sizeof(value), &value);
 
     return 0;
 }
--- bitwise_lib.h
+++ bitwise_lib.h
@@ -10,6 +10,6 @@
 #define UINT8_BIG_ENDIAN(x) (x << 4)
 #define U32_BIG_ENDIAN(x) ((0xFF000000 & x) >> 24) | ((0x00FF0000 & x) >> 8) | ((0x0000FF00 & x) << 8) | ((0x000000FF & x) << 24)
 
-extern void print_bit(uint8_t x);
+extern void print_bit(size_t const x, void const *ptr);
 
 #endif
