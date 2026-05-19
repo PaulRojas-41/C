0590e55f615c55e19de90f2d093f55462c59a7d2
Paul Rojas <sirpaul.tapia@hotmail.com>
2026-05-19 21:51:30 +0200

    [flash_snippet] Flash snippet
    This commit contains a quick flash snippet to understand in the scenario
    of writing data to an specific hw memory area

--- x3_pointers.c
+++ x3_pointers.c
@@ -11,8 +11,8 @@ int main()
     int *ptr = test; /* test represents the address of the first element of the array => test[0]*/
     int *ptr_2 = &test[0]; /* Another way to initialize the pointer with the address of the first element of the array */
 
-    char *names[] = {"Bienvenue", "Paul", "Rojas"};
-    char *get_string = names[0];
+    char *names[] = {"Bienvenue", "Paul", "Rojas"}; //array of pointers initialization.
+    char *get_string = names[0]; // pointer of type char initializes with the first pointer of the array (names[0])
 
     uint8_t i,j;
 
@@ -31,6 +31,21 @@ int main()
         for(j = 0; j < strlen(names[i]); j++)
             printf("%c", *(get_string + j)); 
     }
+
+    /* Array of pointers section */
+
+    uint32_t u32_data_format = 0x08008000;
+    volatile uint32_t *u32_ptr_data = (volatile uint32_t *)u32_data_format; // address to write
+    uint32_t array[] = {0xCC, 0xEE, 0xAA};
+
+    printf("\n%x", u32_ptr_data);
+
+    
+    *u32_ptr_data = (uint32_t)array[0]; //we write in "0x08008000"
+
+    printf("\n%x", u32_ptr_data); //address = 0x8008000
+    printf("\n%x", *u32_ptr_data); //content = CC
+
 
     return 0;
 }
